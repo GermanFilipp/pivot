abc1dea5ed8d67f18b9c2d54d50dd3911b816478
Herman Filippovych <germanfilipp@gmail.com>
2020-01-06 20:53:45 +0200

    add code to Pivot::Item

--- lib/pivot/item.rb
+++ lib/pivot/item.rb
@@ -1,5 +1,25 @@
 module Pivot
   class Item
-    # Code here!
+    VALID_PREFIXES = %W(EREC AZR).freeze
+    private_constant :VALID_PREFIXES
+    def initialize(name:, assignee:, points:)
+      @name = name
+      @assignee = assignee
+      @points = points
+    end
+
+    attr_reader :name, :assignee, :points
+    
+    def +(item)
+      points + item.points
+    end
+    
+    def project_code
+      name.split("-").first
+    end
+    
+    def valid?
+      VALID_PREFIXES.include? project_code
+    end
   end
 end
